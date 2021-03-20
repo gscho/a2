3ca1de52ec7028420b8ab676e28743bba7e517b8
gscho <greg.c.schofield@gmail.com>
2021-03-20 16:21:28 -0400

    Rename env vars
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- lib/a2/client.rb
+++ lib/a2/client.rb
@@ -1,15 +1,17 @@
+require_relative 'client/managed_nodes'
 require_relative 'client/teams'
 require_relative 'client/users'
 
 module A2
   class Client
+    include A2::Client::ManagedNodes
     include A2::Client::Teams
     include A2::Client::Users
 
     def initialize(opts = {})
-      @automate_url = opts[:automate_url] || ENV['A2_AUTOMATE_URL']
-      @automate_token = opts[:automate_token] || ENV['A2_AUTOMATE_TOKEN']
-      @ssl_no_verify = opts[:ssl_no_verify] || ENV['A2_SSL_NO_VERIFY'] || false
+      @automate_url = opts[:automate_url] || ENV['AUTOMATE_URL']
+      @automate_token = opts[:automate_token] || ENV['AUTOMATE_TOKEN']
+      @ssl_no_verify = opts[:ssl_no_verify] || ENV['AUTOMATE_SSL_NO_VERIFY'] || false
 
       raise A2::Error, "Must provide the URL for Chef Automate" if @automate_url.nil?
       raise A2::Error, "Must provide a token for Chef Automate" if @automate_token.nil?
