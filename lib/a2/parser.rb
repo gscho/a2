08d1aabb33ad527844ddd3b2987db42946428c96
gscho <greg.c.schofield@gmail.com>
2021-03-20 16:21:01 -0400

    Add version command
    Signed-off-by: gscho <greg.c.schofield@gmail.com>

--- lib/a2/parser.rb
+++ lib/a2/parser.rb
@@ -2,8 +2,11 @@ module A2
   module Parser
     def parse!
       parser = CmdParse::CommandParser.new(handle_exceptions: true)
+      parser.main_options.version = A2::VERSION
       parser.add_command(CmdParse::HelpCommand.new)
+      parser.add_command(CmdParse::VersionCommand.new)
       parser.add_command(A2::Command::IAM.new)
+      parser.add_command(A2::Command::Node.new)
       parser.global_options do |opt|
         opt.on("-u", "--automate-url [url]", "The Chef Automate URL") do |url|
           parser.data[:automate_url] = url
